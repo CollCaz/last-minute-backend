fb8a1ffb0f5afc4fde802375df23bcf3908b2ca3
CollCaz <yassentombok@proton.me>
2024-12-21 23:42:35 +0200

    can compile and run c++  code

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +15,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
 
 	r.GET("/", s.HelloWorldHandler)
+	r.POST("/", s.SubmitSolution)
 
 	return r
 }
@@ -19,4 +25,37 @@ func (s *Server) HelloWorldHandler(c *gin.Context) {
 	resp["message"] = "Hello World"
 
 	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) SubmitSolution(c *gin.Context) {
+	// _, err := io.ReadAll(c.Request.Body)
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, err.Error())
+	// 	return
+	// }
+	body := c.Request.Body
+	codeBytes, err := io.ReadAll(body)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+	f, _ := os.CreateTemp("", "*_code.cpp")
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	_, err = f.Write(codeBytes)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+
+	outFile, _ := os.CreateTemp("", "*_code.out")
+	compileCmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("g++ -o %s %s", outFile.Name(), f.Name()))
+	runCmd := exec.Command(outFile.Name())
+	_, _ = compileCmd.CombinedOutput()
+	f.Close()
+	outFile.Close()
+	o, _ := runCmd.CombinedOutput()
+
+	c.JSON(http.StatusOK, string(o))
 }
